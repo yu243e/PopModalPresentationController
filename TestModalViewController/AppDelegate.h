a33c6b91f8d191d94d4f9e85e5caf182d751855b
yu243e <yu243e@gmail.com>
2016-08-15 15:58:30 +0800

    Initial Commit

---
+++ TestModalViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestModalViewController
+//
+//  Created by yu243e on 16/8/15.
+//  Copyright © 2016年 yu243e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestModalViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestModalViewController
+//
+//  Created by yu243e on 16/8/15.
+//  Copyright © 2016年 yu243e. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
